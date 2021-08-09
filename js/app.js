51a8e5f552a40058fb87eae34ab757bb6d89ed93
CarlosFarfan29 <cjfarfana@unac.edu.pe>
2021-08-09 02:32:02 -0500

    Lorem

---
+++ js/app.js
@@ -0,0 +1,72 @@
+window.sr = ScrollReveal();
+
+  sr.reveal('.navbar', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.logo', {
+    delay: 2400,
+    rotate: {
+        x: 1,
+        y: 180,
+    },
+  });
+
+  sr.reveal('.textos-portada', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });
+
+  sr.reveal('.contenedor-logos', {
+    duration: 4000,
+    origin: 'left',
+    distance: '400px'
+  });
+
+  sr.reveal('.scroll-cursos', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '300px'
+  });
+
+  sr.reveal('.caract', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '200px'
+  });
+
+  sr.reveal('.owl-carousel', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '200px'
+  });
+
+  sr.reveal('.scroll-titulo', {
+    duration: 4000,
+    origin: 'left',
+    distance: '100px'
+  });
+
+  sr.reveal('.form', {
+    duration: 4000,
+    origin: 'left',
+    distance: '200px'
+  });
+
+  sr.reveal('.img-form', {
+    duration: 4000,
+    origin: 'right',
+    distance: '200px'
+  });
+
+  sr.reveal('.contenedor-footer', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '100px'
+  });
+  
+
+  
